1d2f452bfe33130f5a1b9341ad807082bc9701f2
Irina <vyustirina@gmail.com>
2025-03-01 22:49:15 +0300

    added redux

--- lesson1/src/main.jsx
+++ lesson1/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import store from './redux/store.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+
+  </Provider>
 )
