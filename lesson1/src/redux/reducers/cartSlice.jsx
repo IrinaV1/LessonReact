cdf71d82ad376e73c02c9ab44c716047edb7b27c
Irina <vyustirina@gmail.com>
2025-03-31 23:41:37 +0300

    added localStorage

--- lesson1/src/redux/reducers/cartSlice.jsx
+++ lesson1/src/redux/reducers/cartSlice.jsx
@@ -1,12 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit"
 
+const loadCartFromLocalStorage = () => {
+    const data = localStorage.getItem('cart');
+    return data ? JSON.parse(data) : [];
+};
+
+
 const initialState = {
     items: [],
 };
 
 const cartSlice = createSlice({
     name: "cart",
-    initialState,
+    initialState: {
+        items: loadCartFromLocalStorage(),
+    },
 reducers: {
     addToCart: (state, action) => {
         const existingItem = state.items.find((item) => item.id === action.payload.id)
@@ -15,10 +23,26 @@ reducers: {
     } else {
         state.items.push({...action.payload, quantity: 1});
     }
+    localStorage.setItem("cart", JSON.stringify(state.items));
     },
     removeFromCart: (state, action) => {
     state.items = state.items.filter((item) => item.id !== action.payload);
+    localStorage.setItem("cart", JSON.stringify(state.items));
+},
+    increaseQuantity: (state, action) => {
+const item = state.items.find((item) => item.id === action.payload);
+if(item) {
+    item.quantity += 1;
+}
+localStorage.setItem("cart", JSON.stringify(state.items));
     },
+    decreaseQuantity: (state, action) => {
+const item = state.items.find((item) => item.id === action.payload);
+if(item && item.quantity > 1) {
+    item.quantity -= 1;
+}
+localStorage.setItem("cart", JSON.stringify(state.items));
+    }
 }
 })
 
